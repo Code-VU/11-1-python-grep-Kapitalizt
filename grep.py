e68a9594970f4c143a5ebf53322be0ae251c573c
Rob Kirchoff <Rob.Kirchoff@vu.com>
2022-05-16 17:26:41 -0500

    1st

--- grep.py
+++ grep.py
@@ -6,6 +6,19 @@ import sys
 def countpatterninfile():
     regular_expression = input("Enter a regular expression: ")
 
+    file_name = "mbox-long.txt"
+    fhand = open(file_name)
+    re_count = 0
+
+    for line in fhand:
+        #line = line.rstrip()
+        if re.findall(regular_expression, line):
+            re_count += 1
+    
+    print (f'mbox.txt had {re_count} lines that matched {regular_expression}')
+        
+
+
 if __name__ == '__main__':
     # this is called a main method
     # This is another way of telling python THIS IS WHERE YOU SHOULD START RUNNING
